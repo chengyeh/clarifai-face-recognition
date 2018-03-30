2f8e3b6e5224af72628eec18504cfcb8d230e654
Cheng Yeh Lee <chengyeh@ku.edu>
2018-03-30 05:01:46 -0500

    Implements form validation to check for proper email and password

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
+import FormErrors from '../FormErrors/FormErrors'
 
 class Signin extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			signInEmail: '',
-			signInPassword: ''
+			email: '',
+			password: '',
+			formErrors: {email: '', password: ''},
+		    emailValid: false,
+		    passwordValid: false,
+		    formValid: false
 		}
 	}
 
-	onEmailChange = (event) => {
-		this.setState({signInEmail: event.target.value});
+	handleUserInput = (event) => {
+		const name = event.target.name;
+		const value = event.target.value;
+		this.setState({[name]: value}, () => {
+			this.validateField(name, value);
+		});
 	};
 
-	onPasswordChange = (event) => {
-		this.setState({signInPassword: event.target.value});
+	validateField = (fieldName, value) => {
+		let fieldValidationErrors = this.state.formErrors;
+  		let emailValid = this.state.emailValid;
+  		let passwordValid = this.state.passwordValid;
+
+  		switch(fieldName) {
+  			case 'email':
+  				emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+  				fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+  				break;
+  			case 'password':
+  				passwordValid = value.length >= 6;
+  				fieldValidationErrors.password = passwordValid ? '': ' must be at least 6 characters';
+  				break;
+  			default:
+  				break;
+  		}
+
+  		this.setState({
+  			formErrors: fieldValidationErrors,
+  			emailValid,
+  			passwordValid
+  		}, this.validateForm)
+	};
+
+	validateForm = () => {
+		this.setState({formValid: this.state.emailValid && this.state.passwordValid});
 	};
 
 	onSubmit = (event) => {
@@ -22,8 +56,8 @@ class Signin extends Component {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
-				email: this.state.signInEmail,
-				password: this.state.signInPassword
+				email: this.state.email,
+				password: this.state.password
 			})
 		})
 			.then(response => response.json())
@@ -38,20 +72,29 @@ class Signin extends Component {
 
 	render() {
 		const { onRouteChange } = this.props;
+		const { email, password} = this.state.formErrors;
 		return(
 			<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center interact">
 				<main className="pa4 black-80">
 				  <div className="measure">
 				    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 				      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+				      {
+				      	(email || password) ?
+				      		<div className="f6 ba b--black-10 mw5">
+								<FormErrors formErrors={this.state.formErrors} />
+					  		</div>
+					  	:
+					  	<div></div>
+				      }
 				      <div className="mt3">
-				        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+				        <label className="db fw6 lh-copy f6" htmlFor="email">Email</label>
 				        <input 
 				        	className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
 				        	type="email" 
-				        	name="email-address"  
-				        	id="email-address" 
-				        	onChange={this.onEmailChange}
+				        	name="email"  
+				        	id="email" 
+				        	onChange={this.handleUserInput}
 				        />
 				      </div>
 				      <div className="mv3">
@@ -60,17 +103,20 @@ class Signin extends Component {
 				        	className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
 				        	type="password" 
 				        	name="password"  
-				        	id="password" 
-				        	onChange={this.onPasswordChange}
+				        	id="password"
+				        	onChange={this.handleUserInput}
 				        />
 				      </div>
 				    </fieldset>
-				    <div className="">
-				      <input 
+				    <div>
+				      <button 
 				      	className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
 				      	type="submit" 
-				      	value="Sign in" 
-				      	onClick={this.onSubmit} />
+				      	onClick={this.onSubmit} 
+				      	disabled={!this.state.formValid}
+				      >
+				      		Sign in
+				      </button>
 				    </div>
 				    <div className="lh-copy mt3">
 				      <p 
