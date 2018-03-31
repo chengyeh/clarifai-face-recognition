43761a5aefc7d232a4ecbc2bff94d0f464120098
Cheng Yeh Lee <chengyeh@ku.edu>
2018-03-31 02:58:00 -0500

    Removes console.log

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -82,7 +82,6 @@ class Signup extends Component {
 	};
 
 	render() {
-		console.log(this.state);
 		const { name, email, password} = this.state.formErrors;
 		return(
 			<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center interact">
